ea01094d18fb85a0bae29ddb85d9de19c1e66273
Ehsan Zilaei <zzilaei@gmail.com>
2022-01-18 11:29:27 +0100

    ts interface for date range component

---
+++ src/interfaces/IDateRange.ts
@@ -0,0 +1,11 @@
+import moment from "moment";
+
+export interface IDateRange {
+  startDate: null | moment.Moment;
+  endDate: null | moment.Moment;
+}
+
+export interface IDateRangePickerProps {
+  onValueChange: (selectedDates: IDateRange) => void;
+  enabledDateRange?: IDateRange;
+}
