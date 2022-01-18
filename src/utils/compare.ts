aea96bd6ddf6b701ac401ad5f083dbeb5c0c014e
Jonatan Hanson <jonatan.hanson@gmail.com>
2022-01-18 09:18:34 +0100

    added return type to compare method

--- src/utils/compare.ts
+++ src/utils/compare.ts
@@ -1,14 +1,28 @@
-const compare = (param1: string|boolean|number, operator: string, param2: string|boolean|number) => {
+const compare = (
+  param1: string | boolean | number,
+  operator: string,
+  param2: string | boolean | number
+): boolean => {
   switch (operator) {
-    case '>': return param1 > param2;
-    case '<': return param1 < param2;
-    case '>=': return param1 >= param2;
-    case '<=': return param1 <= param2;
-    case '==': return param1 == param2;
-    case '!=': return param1 != param2;
-    case '===': return param1 === param2;
-    case '!==': return param1 !== param2;
+    case ">":
+      return param1 > param2;
+    case "<":
+      return param1 < param2;
+    case ">=":
+      return param1 >= param2;
+    case "<=":
+      return param1 <= param2;
+    case "==":
+      return param1 == param2;
+    case "!=":
+      return param1 != param2;
+    case "===":
+      return param1 === param2;
+    case "!==":
+      return param1 !== param2;
+    default:
+      return false;
   }
-}
+};
 
 export default compare;
