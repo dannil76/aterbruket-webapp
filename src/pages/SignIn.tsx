f00778d9c1f6e67b86f08e3dc397841746acd406
Ammelie Neth <22522758+Ammelie@users.noreply.github.com>
2022-11-11 16:38:55 +0100

    make username lowercase at sign in (#243)
    Co-authored-by: Ammelie Neth <ammelie.neth@vntrs.com>

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,34 +1,38 @@
-import React, { FC } from "react";
-import { AmplifySignIn } from "@aws-amplify/ui-react";
-import { withRouter } from "react-router-dom";
+import React, { FC } from 'react';
+import { AmplifySignIn } from '@aws-amplify/ui-react';
+import { withRouter } from 'react-router-dom';
 
 const SignIn: FC = ({ children }) => {
-  return (
-    <>
-      {children}
-      <AmplifySignIn
-        headerText=""
-        submitButtonText="Logga in"
-        usernameAlias="username"
-        hideSignUp
-        formFields={[
-          {
-            type: "username",
-            label: "Användarnamn",
-            placeholder: "Ditt användarnamn",
-            required: true,
-          },
-          {
-            type: "password",
-            label: "Lösenord",
-            placeholder: "Fyll i ditt lösenord",
-            required: true,
-            hint: "", // <-- Hides forgotten password link
-          },
-        ]}
-      />
-    </>
-  );
+    return (
+        <>
+            {children}
+            <AmplifySignIn
+                headerText=""
+                submitButtonText="Logga in"
+                usernameAlias="username"
+                hideSignUp
+                formFields={[
+                    {
+                        type: 'username',
+                        label: 'Användarnamn',
+                        placeholder: 'Ditt användarnamn',
+                        required: true,
+                        handleInputChange: (event: Event) => {
+                            const target = event.target as HTMLInputElement;
+                            target.value = target.value.toLowerCase();
+                        },
+                    },
+                    {
+                        type: 'password',
+                        label: 'Lösenord',
+                        placeholder: 'Fyll i ditt lösenord',
+                        required: true,
+                        hint: '', // <-- Hides forgotten password link
+                    },
+                ]}
+            />
+        </>
+    );
 };
 
 export default withRouter(SignIn);
