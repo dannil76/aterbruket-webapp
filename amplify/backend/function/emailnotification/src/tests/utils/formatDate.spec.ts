45508c22795f38b35e74e0ed1f13d058f44b8360
MrSpex <mail@dannilsson.se>
2023-05-10 08:00:37 +0200

    tests(emailnotification): add missing ’kl.’

--- amplify/backend/function/emailnotification/src/tests/utils/formatDate.spec.ts
+++ amplify/backend/function/emailnotification/src/tests/utils/formatDate.spec.ts
@@ -9,7 +9,7 @@ describe('Format datetimes', () => {
     it('handle correct date', () => {
         const date = new Date('2022-01-01');
         const actual = formatDateTime(date);
-        expect(actual).toBe('lördag 1 januari 2022 01:00');
+        expect(actual).toBe('lördag 1 januari 2022 kl. 01:00');
     });
 });
 
